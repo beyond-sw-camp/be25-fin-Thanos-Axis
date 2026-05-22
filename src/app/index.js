362c40850958489963b6eebe5e01d8c233de09d5
penep0 <pengco681@gmail.com>
2026-05-22 18:59:26 +0900

    feat: 폰트 변경

--- src/app/index.js
+++ src/app/index.js
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 import App from '../App.vue'
 import router from '../router'
 import { pinia } from '../stores'
+import '@fontsource/inter/latin-400.css'
+import '@fontsource/inter/latin-500.css'
+import '@fontsource/inter/latin-600.css'
 import './styles/main.css'
 
 export function createMeetBowlApp() {
